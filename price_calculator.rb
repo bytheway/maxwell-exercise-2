fd84a20d6e7344365175dff114fb19bce0f29983
Ben Bytheway <bbytheway@gmail.com>
2021-07-28 23:26:39 -0600

    rename variable to better reflect purpose

--- price_calculator.rb
+++ price_calculator.rb
@@ -5,11 +5,11 @@ require_relative 'item'
 require_relative 'cart'
 
 
-items = {}
-items['milk'] = Item.new(name: 'Milk', unit_price: "3.97", sale_price: "5.00", sale_qty: 2 )
-items['bread'] =  Item.new(name: 'Bread', unit_price: "2.17", sale_price: "6.00", sale_qty: 3 )
-items['banana'] =Item.new(name: 'Banana', unit_price: "0.99")
-items['apple'] = Item.new(name: 'Apple', unit_price: "0.89")
+price_list = {}
+price_list['milk'] = Item.new(name: 'Milk', unit_price: "3.97", sale_price: "5.00", sale_qty: 2 )
+price_list['bread'] =  Item.new(name: 'Bread', unit_price: "2.17", sale_price: "6.00", sale_qty: 3 )
+price_list['banana'] =Item.new(name: 'Banana', unit_price: "0.99")
+price_list['apple'] = Item.new(name: 'Apple', unit_price: "0.89")
 
 puts "Please enter all the items purchased separated by a comma"
 
@@ -17,7 +17,7 @@ input = gets.chomp
 
 input = input.split(",").map{|i| i.strip}
 
-cart = Cart.new(items)
+cart = Cart.new(price_list)
 
 input.each do |i|
     next if i.nil? || i.empty?
